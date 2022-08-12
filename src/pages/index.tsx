d9420dd4741ddc9766e665c0aadc82e18d93c332
Eduardo Ferraz <edupferraz@gmail.com>
2022-08-12 14:57:23 -0300

    Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
@@ -8,7 +10,24 @@ export default function Home() {
         <title>Home | Ig News</title>
       </Head>
 
-      <h1>Hello World </h1>
+      <main className={styles.contentContainer}>
+
+        <section className={styles.hero}>
+          
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+
+          <p>
+            Get access to all the publication <br />
+            <span>for $9.90 month</span>
+          </p>
+
+
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+
+      </main>
 
     </>
   )
