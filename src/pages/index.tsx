3ad6eb17036dd15933fd4f09c741a6eb291b1c46
Eduardo Ferraz <edupferraz@gmail.com>
2022-08-13 20:41:15 -0300

    Subscribe Button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
@@ -22,6 +23,7 @@ export default function Home() {
             <span>for $9.90 month</span>
           </p>
 
+          <SubscribeButton/>
 
         </section>
 
