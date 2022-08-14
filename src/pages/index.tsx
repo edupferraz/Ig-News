2c2ac2c157a8cb9c8811170601e4e8c1786341ab
Eduardo Ferraz <edupferraz@gmail.com>
2022-08-13 21:32:10 -0300

    Static Site Generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -45,7 +45,7 @@ export default function Home({product}: HomeProps) {
 }
 
 // run on next
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1LWU1UKHGhtRnBbeu1dCdc86', {
     expand: ['product']
@@ -62,6 +62,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return{
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
